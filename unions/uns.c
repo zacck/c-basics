3fc99eca4de056c161abf7a32b92ea1246c3ef92
Zacck Osiemo <coderv63@gmail.com>
2023-10-02 07:16:44 +0200

    Introduce unions

---
+++ unions/uns.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <stdio.h>
+
+union Address {
+  uint16_t shortAddr;
+  uint32_t longAddr;
+};
+
+int main(void) {
+  union Address addr;
+
+  addr.shortAddr = 0xABCD;
+  // addr.longAddr = 0xEEEECCCC;
+
+  printf("short addr = %#X\n", addr.shortAddr);
+  printf("long addr = %#X\n", addr.longAddr);
+  return 0;
+}
